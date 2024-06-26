831d6b9f4ab0854463bfffc53db1f9e5552bd4ff
xuliang <xuliangwien@gmail.com>
2024-06-26 11:11:54 +0200

    Titel-modified

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Simple VitePress Site",
+  title: "Welcome to our Web Application ",
   description: "Training Page for Cloud Computing Course",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
